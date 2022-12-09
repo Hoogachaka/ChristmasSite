fb688e5c2b00256f8a02d213ea9c2d7689325da0
Hoogachaka <thatdylan@gmail.com>
2022-12-08 21:52:29 -0600

    site ready for Data

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const flyAwayFunc = () => {
+    document.getElementById("mainBody").classList.add('flyAway');
+}
+const link = () => {
+    window.location.href = "/pages/nav.html"
+}
+const superLink = (location) => {
+    window.location.href = location
+}
