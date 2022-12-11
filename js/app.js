df6087f76409e888e6d8ee0717439df0679289e0
Hoogachaka <thatdylan@gmail.com>
2022-12-10 20:21:20 -0600

    tried to fix links for github pages part 2

--- js/app.js
+++ js/app.js
@@ -5,5 +5,5 @@ const link = () => {
     window.location.href = "/pages/nav.html"
 }
 const superLink = (location) => {
-    window.location.href = "/ChristmasSite" + location
+    window.location.href = location
 }
